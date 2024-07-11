a7fcb7592a18edf760bbe06ecc9b329c4d57283e
Daniel Thielmann <thielmanndan@gmail.com>
2024-07-11 17:25:20 -0300

    build pronta, começando projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-2xl">
+       <h1>Site Semana Comp UFJF 2024</h1>
+      </div>
+    </main>
+  );
+}
