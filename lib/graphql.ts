9f630c4f4da6731d91ba8a303f2b6199ac35ff02
Daniel Thielmann <thielmanndan@gmail.com>
2024-07-11 17:28:32 -0300

    bug fixed

--- lib/graphql.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-
-const endpoint = 'https://api.hygraph.com/v2/your-project-id/master'; // Substitua pela URL do hygraph 
-
-export const hygraph = new GraphQLClient(endpoint, {
-  headers: {
-    authorization: `Bearer YOUR_ACCESS_TOKEN`, // Substitua pelo seu token de acesso hygraph
-  },
-});
