95a1e098006e7dba8a244e2c9233da3e7c486620
Lucas <lucaspaiva@live.com>
2024-07-21 20:36:10 -0300

    adiciona background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 import Navbar from "@/components/navBar";
 
 export default function Home() {
-  return <Navbar />;
+  const backgroundImage = "/images/fundoPostEscuro.png";
+  return (
+    <div
+      className="h-screen w-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Navbar />
+
+      <div>
+        <h1>XXVI</h1>
+        <h1>Semana da Computação</h1>
+      </div>
+    </div>
+  );
 }
