a2938c5991a9fcac8566b462d2c0e3c9dd910f1f
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-07-23 12:49:53 -0300

    improve click

--- src/components/faq-model.tsx
+++ src/components/faq-model.tsx
@@ -11,9 +11,9 @@ export default function FaqModel({ faq }: FaqModelProps) {
   const [isQuestionOpen, setIsQuestionOpen] = useState(false);
 
   return (
-    <div className="flex flex-col border lg:p-16 md:p-12 p-8 border-black">
+    <div className="flex flex-col border  border-black">
       <div
-        className="flex justify-between cursor-pointer"
+        className="flex justify-between cursor-pointer lg:p-16 md:p-12 p-8"
         onClick={() => setIsQuestionOpen(!isQuestionOpen)}
       >
         <h1 className="lg:text-2xl md:text-xl text-base font-semibold">
@@ -29,8 +29,10 @@ export default function FaqModel({ faq }: FaqModelProps) {
       </div>
 
       <span
-        className={`lg:text-2xl md:text-xl text-base overflow-x-auto  duration-300 transition-max-height  ${
-          isQuestionOpen ? "max-h-screen mt-10" : "max-h-0"
+        className={`lg:text-2xl md:text-xl text-base overflow-x-auto lg:px-16 md:px-12 px-8 duration-300 transition-max-height  ${
+          isQuestionOpen
+            ? "max-h-screen lg:-mt-8 md:-mt-6 -mt-4 lg:py-16 md:py-12 py-8"
+            : "max-h-0"
         }`}
         dangerouslySetInnerHTML={{ __html: faq.answer }}
       ></span>
