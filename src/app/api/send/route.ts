dba4ff37c8194a71c39a34920fe73142f66bc0c6
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-08-06 01:03:55 -0300

    package.json fix

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,9 +2,8 @@ import { EmailForm } from "@/schema/email.form";
 import TwitchResetPasswordEmail from "../../../components/contact/email-template";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request): Promise<Response> {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body: EmailForm = await request.json();
     const { data, error } = await resend.emails.send({
@@ -22,16 +21,16 @@ export async function POST(request: Request): Promise<Response> {
       }) as React.ReactElement,
     });
 
-    const confirmationEmail = await resend.emails.send({
-      from: `Acme <no-reply@${process.env.RESEND_EMAIL_DOMAIN}>`,
-      to: [body.email],
-      subject: "Confirmação de envio",
-      react: TwitchResetPasswordEmail({
-        emailContent: `Seu email foi enviado com sucesso para ${body.email}`,
-        subject: "Confirmação de envio",
-        userEmail: `no-reply@${process.env.RESEND_EMAIL_DOMAIN}`,
-      }) as React.ReactElement,
-    });
+    // await resend.emails.send({
+    //   from: `Acme <no-reply@${process.env.RESEND_EMAIL_DOMAIN}>`,
+    //   to: [body.email],
+    //   subject: "Confirmação de envio",
+    //   react: TwitchResetPasswordEmail({
+    //     emailContent: `Seu email foi enviado com sucesso para ${body.email}`,
+    //     subject: "Confirmação de envio",
+    //     userEmail: `no-reply@${process.env.RESEND_EMAIL_DOMAIN}`,
+    //   }) as React.ReactElement,
+    // });
 
     if (error) {
       return Response.json({ error }, { status: 500 });
