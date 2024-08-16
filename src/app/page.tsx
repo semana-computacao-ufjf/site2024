7072b541cb0d0b721c0b1414c448487922d169cc
Lucas <lucaspaiva@live.com>
2024-08-16 07:49:20 -0300

    move as consultas ao banco para o modelo de site contínuo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,92 @@
-import { fakeEventApi, fakeSpeakerApi } from "@/util/fakeApi";
-import { SpeakerData } from "@/types/speaker";
-import Home from "./home";
-import { Event } from "@/types/event";
-import { getAllFaqs } from "./api/faq/faq.question";
+import Home from "../components/home";
+import { Sponsor } from "@/types/sponsor";
+import { Event, Prize } from "@/types/event";
+import { Presenter } from "@/types/presenter";
 import { Faq } from "@/types/faq";
+import { getAllSponsors } from "./api/fetch/sponsor.fetch";
+import { getAllEvents } from "./api/fetch/event.fetch";
+import { getAllPresenters } from "./api/fetch/presenter.fetch";
+import { getAllFaqs } from "./api/faq/faq.question";
 
-const events: Event[] = fakeEventApi();
-
-const speakers: SpeakerData[] = fakeSpeakerApi();
-
+//TODO: as imagens devem estar no banco
+const presentationImages = [
+  "/images/exampleImages/20231025_140937-1-1024x768.jpg",
+  "/images/exampleImages/a68cc476-12f6-4b16-9047-762f860abec1.jpeg",
+  "/images/exampleImages/Porte-dinmica-4.jpg",
+  "/images/exampleImages/Figura-5-Noite-de-jogos-na-Semana-da-ComputacComputacComputacao-da-UFJF-em-2017.png",
+];
+async function getSponsors(): Promise<Sponsor[]> {
+  try {
+    const sponsors = await getAllSponsors();
+    if (!sponsors) {
+      throw new Error("No sponsors found");
+    }
+    return sponsors;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+async function getEvents(): Promise<Event[]> {
+  try {
+    const events = await getAllEvents();
+    if (!events) {
+      throw new Error("No events found");
+    }
+    return events;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+//TODO: os eventos devem conter campos para a premiação
+const prizes: Prize[] = [
+  {
+    title: "Maratona de Programação",
+    firstPlace: "R$ 1000",
+    secondPlace: "R$ 500",
+    thirdPlace: "R$ 250",
+  },
+  {
+    title: "Game Jam",
+    firstPlace: "R$ 1500",
+    secondPlace: "R$ 700",
+    thirdPlace: "R$ 300",
+  },
+  {
+    title: "Competição de CSS",
+    firstPlace: "R$ 2000",
+    secondPlace: "R$ 1000",
+    thirdPlace: "R$ 500",
+  },
+];
+async function getPresenters(): Promise<Presenter[]> {
+  try {
+    const presenters = await getAllPresenters();
+    if (!presenters) {
+      throw new Error("No presenters found");
+    }
+    return presenters;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+//TODO: as perguntas devem estar no banco
 const faqs: Faq[] = getAllFaqs();
-export default function App() {
-  return <Home events={events} speakers={speakers} faqs={faqs} />;
+
+export default async function App() {
+  const sponsors: Sponsor[] = await getSponsors();
+  const events: Event[] = await getEvents();
+  const presenters: Presenter[] = await getPresenters();
+  return (
+    <Home
+      presentationImages={presentationImages}
+      sponsors={sponsors}
+      events={events}
+      prizes={prizes}
+      presenters={presenters}
+      faqs={faqs}
+    />
+  );
 }
