eefdf1f714f6c7c1de287d65499a3bc0a14b4161
Samuel Nascimento <me@samnsc.com>
2024-09-19 14:39:45 -0300

    force dynamic generation of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Home } from "../components/home";
 import { prisma } from "@/util/prisma";
 
+// next was statically generating the main page which meant the database queries
+// were only happening once during compile time which is not what we want
+export const dynamic = "force-dynamic";
+
 const App = async () => {
   const sponsors = await prisma.sponsor.findMany();
   const events = await prisma.event.findMany({
