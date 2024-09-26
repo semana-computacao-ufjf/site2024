e21f518abe6cb5277cba3518bc0c671515531756
Lucas Paiva <lucaspaiva@live.com>
2024-09-26 04:23:56 -0300

    add contact information

--- src/components/navbar footer/Footer.tsx
+++ src/components/navbar footer/Footer.tsx
@@ -13,29 +13,32 @@ export default function Footer() {
       </h1>
       <div className="text-xl sm:text-4xl flex flex-col sm:flex-row justify-start space-y-6 sm:space-y-0 sm:space-x-6 ml-8 sm:ml-[168px] pt-16 sm:pb-14">
         <div className="flex items-center space-x-4 pb-3 sm:pb-0">
-          <a href="tel:+5524999114310" className="text-3xl">
+          <a href="tel:+553221023327" className="text-3xl">
             <PhoneIcon style={{ color: "white", fontSize: "45px" }} />
           </a>
-          <span>(xx) xxxx-xxxx</span>
+          <span>(32) 2102-3327</span>
         </div>
 
         <div className="flex items-center space-x-4 pb-3 sm:pb-0">
-          <a href="mailto:email@example.com" className="text-3xl">
+          <a
+            href="mailto:semana-da-computacao@ice.ufjf.br"
+            className="text-3xl"
+          >
             <EmailIcon style={{ color: "white", fontSize: "45px" }} />
           </a>
-          <span>abcde@gmail.com</span>
+          <span>semana-da-computacao@ice.ufjf.br</span>
         </div>
 
         <div className="flex items-center space-x-4 pb-0 sm:pb-0">
           <a
-            href=""
+            href="https://www.instagram.com/semanadacomputacaoufjf/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-3xl"
           >
             <InstagramIcon style={{ color: "white", fontSize: "45px" }} />
           </a>
-          <span>@intermidiaufv</span>
+          <span>@semanadacomputacaoufjf</span>
         </div>
       </div>
     </footer>
